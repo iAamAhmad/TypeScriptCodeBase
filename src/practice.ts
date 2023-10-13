3672ae11414775db4787a78dab7b50790300ad8d
iAamAhmad <aghazi1011@gmail.com>
2023-10-13 15:14:32 +0500

    practice

--- src/practice.ts
+++ src/practice.ts
@@ -68,31 +68,31 @@ const soldierAges = [90, 19, 45];
 const conjoined = [...soldiers, ...soldierAges];
 console.log(conjoined);
 */
-interface Book {
-  author?: string;
-  pages: number;
-}
-// Ok
-const ok: Book = {
-  author: "Rita Dove",
-  pages: 80,
-};
-const missing: Book = {
-  pages: 80,
-};
+// interface Book {
+//   author?: string;
+//   pages: number;
+// }
+// // Ok
+// const ok: Book = {
+//   author: "Rita Dove",
+//   pages: 80,
+// };
+// const missing: Book = {
+//   pages: 80,
+// };
 
 // Inferred type: string
-let bestSong = Math.random() > 0.5 ? "Chain of Fools" : "Respect";
+// let bestSong = Math.random() > 0.5 ? "Chain of Fools" : "Respect";
 
-let rocker; // Type: any
-rocker = "Joan Jett"; // Type: string
-rocker.toUpperCase(); // Ok
-rocker = 19.58; // Type: number
-rocker.toPrecision(1); // Ok
-// rocker.toUpperCase();
+// let rocker; // Type: any
+// rocker = "Joan Jett"; // Type: string
+// rocker.toUpperCase(); // Ok
+// rocker = 19.58; // Type: number
+// rocker.toPrecision(1); // Ok
+// // rocker.toUpperCase();
 
-let rocker1: string;
-rocker = "Joan Jett";
+// let rocker1: string;
+// rocker = "Joan Jett";
 // rocker1 = 12;
 
 // let mathematician = Math.random() > 0.5
@@ -108,31 +108,86 @@ rocker = "Joan Jett";
 // admiral.toUpperCase(); // Ok: string
 // admiral.toFixed();
 
-let scientist = Math.random() > 0.5
-? "Rosalind Franklin"
-: 51;
-if (scientist === "Rosalind Franklin") {
-// Type of scientist: string
-scientist.toUpperCase(); // Ok
-}
-// Type of scientist: number | string
-// scientist.toUpperCase();
-let mathematician: string | undefined;
-mathematician?.length; // Ok
-mathematician = "Mark Goldberg";
-mathematician.length; 
+// let scientist = Math.random() > 0.5 ? "Rosalind Franklin" : 51;
+// if (scientist === "Rosalind Franklin") {
+//   // Type of scientist: string
+//   scientist.toUpperCase(); // Ok
+// }
+// // Type of scientist: number | string
+// // scientist.toUpperCase();
+// let mathematician: string | undefined;
+// mathematician?.length; // Ok
+// mathematician = "Mark Goldberg";
+// mathematician.length;
 
-type Poet = {
-  born: number;
-  name: string;
-  };
-  let poetLater: Poet;
-  // Ok
-  poetLater = {
-  born: 1935,
-  name: "Sara Teasdale",
-  };
-  poetLater = "Emily Dickinson";
+// type Poet = {
+//   born: number;
+//   name: string;
+// };
+// let poetLater: Poet;
+// // Ok
+// poetLater = {
+//   born: 1935,
+//   name: "Sara Teasdale",
+// };
+// poetLater = "Emily Dickinson";
+// poetLater = "Emily Dickinson";
+
+// let type1 = { name: "zia", id: 1 };
+// let type2 = { id: 2, name: "zia", age: 22 };
+// console.log((type1 = type2));
+
+
+// type one=(string)=>void;
+// function two(f:one){
+//   f("here")
+// }
+// var oneFunc=f=>{this.x="x"}
+
+// var three=((a,b)=>++a)(1,2);
+// console.log(three);
+
+// var a:{foo:number};
+// var a1={foo:1,baz:2};
+// console.log(a=a1);
+
+// let type1 = { name: "zia", id: 1 };
+// type1 = {  name: "tom", id: 2};
+// type1 = {   id: 2, name: "jack",};
+
+
+// var f:(string)=>string;
+// f=x=>('+ X +');
+// var h:((string)=>string)[]
+
+// function one(arg1: string|number|boolean): string|number|boolean{
+//   if(typeof arg1==="string"){
+//     return arg1+ "hello";
+//   }
+//   if(typeof arg1==="number"){
+//     return arg1+ 10;
+//   }
+//   if(typeof arg1==="boolean"){
+//     return arg1 && false;
+//   }
+// }
+
+
+// var x:any="ahmad";
+// if (typeof x==="string"){
+//   console.log(x.length);
+// }
+// x.unknown();
+
+// let x:any={name:"ahmad", id:1};
+// x={id:2, name:"falak"};
+// x={id:2, name:"falak", lastName:"foo"};
+// x={id:2, name:false};
+let array1: number[]=[2,3,4];
+let array2:Array<number> =[1,23,3];
+let array3=new Array[number];
+let array5:number[]=new number[5]
+
 
 
 
